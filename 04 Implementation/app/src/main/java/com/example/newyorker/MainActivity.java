26db1c006a0b8dafd2f76fd821e6a94e05a9e162
MBRzealand <mikk24f6@edu.easj.dk>
2021-05-27 15:40:43 +0200

    Added customize order activity
    made goToCustomizeOrder()

--- 04 Implementation/app/src/main/java/com/example/newyorker/MainActivity.java
+++ 04 Implementation/app/src/main/java/com/example/newyorker/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.newyorker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void goToCustomizeOrder(View view) {
+
+        Intent intent = new Intent(this, CustomizeOrderActivity.class);
+
+        startActivity(intent);
+
+    }
+
 }
