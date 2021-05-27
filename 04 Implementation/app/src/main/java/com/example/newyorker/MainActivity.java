64ac0f98001832f4eca6567d517472478856a7d2
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2021-05-27 16:01:01 +0200

    Changed back the contentview to main

--- 04 Implementation/app/src/main/java/com/example/newyorker/MainActivity.java
+++ 04 Implementation/app/src/main/java/com/example/newyorker/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity__send__requst);
+        setContentView(R.layout.activity_main);
     }
 }
