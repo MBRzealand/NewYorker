4457d4604d0f7293919c434b2d3f8147e46df3b4
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2021-05-28 12:34:26 +0200

    Added functionality to Email class

--- 04 Implementation/app/src/main/java/com/example/newyorker/model/Email.java
+++ 04 Implementation/app/src/main/java/com/example/newyorker/model/Email.java
@@ -1,8 +1,35 @@
 package com.example.newyorker.model;
 
+import java.util.concurrent.ExecutionException;
+
+import uk.co.jakebreen.sendgridandroid.SendGrid;
+import uk.co.jakebreen.sendgridandroid.SendGridMail;
+import uk.co.jakebreen.sendgridandroid.SendGridResponse;
+import uk.co.jakebreen.sendgridandroid.SendTask;
+
 public class Email {
 
 
+    public void sendEmail() {
+        uk.co.jakebreen.sendgridandroid.SendGrid sendGrid = SendGrid.create("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");
+        SendGridMail mail = new SendGridMail();
+        mail.addRecipient("fredrikbille@hotmail.com", "Fredrik");
+        mail.setFrom("NYWallBuilder@gmail.com", "András");
+        mail.setSubject("Hej fra sendgrid");
+        mail.setContent("Hej, det virker nu. Måske.");
+        System.out.println("about to send");
+        SendTask task = new SendTask(sendGrid, mail);
+        try {
+            System.out.println("we in try");
+            SendGridResponse response = task.execute().get();
 
-
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (
+                ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
