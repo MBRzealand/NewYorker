18ee4c80773919e386bb7d627a048dd0e4aa79ac
MBRzealand <mikk24f6@edu.easj.dk>
2021-05-28 11:50:21 +0200

    Added Preview Layout

---
+++ 04 Implementation/app/src/main/java/com/example/newyorker/PreviewOrderActivity.java
@@ -0,0 +1,31 @@
+package com.example.newyorker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class PreviewOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview_order);
+    }
+
+    public void goToMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
+
+    }
+
+    public void goToSendRequest(View view) {
+
+        Intent intent = new Intent(this, SendRequestActivity.class);
+
+        startActivity(intent);
+
+    }
+}
