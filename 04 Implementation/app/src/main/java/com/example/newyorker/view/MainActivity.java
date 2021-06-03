e14fe1fcd8da7528ab91b1eabfd5aa8fe2c69b0f
MBRzealand <mikk24f6@edu.easj.dk>
2021-06-03 13:03:51 +0200

    Made widthInput force close keyboard on "done" button click

--- 04 Implementation/app/src/main/java/com/example/newyorker/view/MainActivity.java
+++ 04 Implementation/app/src/main/java/com/example/newyorker/view/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.newyorker.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -19,12 +24,18 @@ public class MainActivity extends AppCompatActivity {
     EditText widthInput;
     TextView textViewMainActivityPrice;
 
+    InputMethodManager imm;
+
     NYBuilderController controller = new NYBuilderController();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
 
         initializeUIElements();
         initializeListeners();
@@ -77,6 +88,26 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+        widthInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+
+
+
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
+                    v.clearFocus();
+
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
 
     }
 
