abcbcdad672b655533fc7cc56de63d34e2bd759d
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2021-06-16 13:04:38 +0200

    Removed ekstra import Co-authored-by: Madictian <70747015+Madictian@users.noreply.github.com>

--- 04 Implementation/app/src/main/java/com/example/newyorker/view/CustomizeOrderActivity.java
+++ 04 Implementation/app/src/main/java/com/example/newyorker/view/CustomizeOrderActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -152,6 +151,7 @@ public class CustomizeOrderActivity extends AppCompatActivity  {
             }
 
         });
+
 
 
 
