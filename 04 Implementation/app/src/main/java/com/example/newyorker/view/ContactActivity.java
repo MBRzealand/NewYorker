1feae06f910c4c879df9bdcbed3e062fbe99899d
Madictian <fredrikbille@hotmail.com>
2021-06-17 10:09:36 +0200

    contact webview added.

---
+++ 04 Implementation/app/src/main/java/com/example/newyorker/view/ContactActivity.java
@@ -0,0 +1,37 @@
+package com.example.newyorker.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.example.newyorker.R;
+
+public class ContactActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+
+        webView = findViewById(R.id.webViewer);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.new-yorker.dk/kontakt/");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+    }
+}
