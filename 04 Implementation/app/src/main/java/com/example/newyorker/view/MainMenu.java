7a72842dbff01729ac4d1c14d42c0d4e6fc7cf7d
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2021-06-17 13:39:01 +0200

    Added a main menu screen

---
+++ 04 Implementation/app/src/main/java/com/example/newyorker/view/MainMenu.java
@@ -0,0 +1,23 @@
+package com.example.newyorker.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.newyorker.R;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+
+
+
+
+
+
+}
